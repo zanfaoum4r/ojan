2c669198a59f085ba70831644f6f3ca67af57451
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-01-23 07:52:22 +0700

    Update redir2.js

--- redir2.js
+++ redir2.js
@@ -4,9 +4,9 @@ var key = document.URL.split("/")[4];
 var id = document.URL.split("/")[5];
 
 if (md == 'deep') {
-  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
+  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+_ADYtvX+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
 } else { 
-  var redir = 'https://s.click.aliexpress.com/e/'+key;
+  var redir = 'https://s.click.aliexpress.com/e/'+_ADYtvX;
 }
 
 setTimeout(
