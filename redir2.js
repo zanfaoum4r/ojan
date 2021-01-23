19e602b53495d81c903d974480741e995e17068f
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-01-23 08:04:15 +0700

    Update redir2.js

--- redir2.js
+++ redir2.js
@@ -1,10 +1,9 @@
 var urlmain = document.URL;
 var md = document.URL.split("/")[3];
-var key = document.URL.split("/")[4];
+var key = '_ADYtvX';
 var id = document.URL.split("/")[5];
 
 if (md == 'deep') {
-  var key = '_ADYtvX';} {
   var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
 } else { 
   var redir = 'https://s.click.aliexpress.com/e/'+key;
