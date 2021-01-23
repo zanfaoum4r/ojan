8ae83cb0860c0fe73a228033374fd2043ff1a015
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-01-23 08:31:07 +0700

    Update redir3.js

--- redir3.js
+++ redir3.js
@@ -3,3 +3,9 @@ var d= window.location.href;
 d=d.replace(&quot;?&quot;, &quot; &quot;);
 d=d.match(/\d{11,20}/);
 location.href = &#39;http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/&#39;+d+&#39;.html&amp;aff_short_key=_ADYtvX&#39;;
+
+setTimeout(
+  function(){
+    window.location = redir
+  },
+1000);
