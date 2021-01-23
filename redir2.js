ea988f6b30ec24c66b002c62e0459b4a06d9f785
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-01-23 08:19:12 +0700

    Update redir2.js

--- redir2.js
+++ redir2.js
@@ -1,13 +1,9 @@
 var urlmain = document.URL;
 var md = document.URL.split("/")[3];
-var key = '_ADYtvX';
+var key = document.URL.split("/")[4];
 var id = document.URL.split("/")[5];
 
-if (md == 'deep') {
-  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
-} else { 
-  var redir = 'https://s.click.aliexpress.com/e/'+key;
-}
+location.href = &#39;http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/&#39;+d+&#39;.html&amp;aff_short_key=KODEDISINI&#39;;
 
 setTimeout(
   function(){
