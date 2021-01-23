bc968ca1a4ef185e0e7ee0280297b275a79a968c
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-01-23 08:26:10 +0700

    Update redir2.js

--- redir2.js
+++ redir2.js
@@ -1,11 +1,1 @@
-var d=&#39;&#39;;
-var d= window.location.href;
-d=d.replace(&quot;?&quot;, &quot; &quot;);
-d=d.match(/\d{11,20}/);
 location.href = &#39;http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/&#39;+d+&#39;.html&amp;aff_short_key=_9IiKUC&#39;;
-
-setTimeout(
-  function(){
-    window.location = redir
-  },
-1000);
