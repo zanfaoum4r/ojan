c2697de324f36aca73f543ffa90f7c605ad34ecf
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-01-23 08:22:48 +0700

    Update redir2.js

--- redir2.js
+++ redir2.js
@@ -1,9 +1,8 @@
-var urlmain = document.URL;
-var md = document.URL.split("/")[3];
-var key = document.URL.split("/")[4];
-var id = document.URL.split("/")[5];
-
-location.href = &#39;http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/&#39;+d+&#39;.html&amp;aff_short_key=_ADYtvX&#39;;
+var d=&#39;&#39;;
+var d= window.location.href;
+d=d.replace(&quot;?&quot;, &quot; &quot;);
+d=d.match(/\d{11,20}/);
+location.href = &#39;http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/&#39;+d+&#39;.html&amp;aff_short_key=_9IiKUC&#39;;
 
 setTimeout(
   function(){
