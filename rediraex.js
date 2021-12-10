dbdcb6843f57caf46fb0c07a70f00652f8e66fcf
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-12-10 13:38:08 +0700

    Update rediraex.js

--- rediraex.js
+++ rediraex.js
@@ -1,5 +1,5 @@
-var d=&#39;&#39;;
+var d='';
 var d= window.location.href;
-d=d.replace(&quot;?&quot;, &quot; &quot;);
+d=d.replace("?", " ");
 d=d.match(/\d{11,20}/);
-location.href = &#39;http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/&#39;+d+&#39;.html&amp;aff_short_key=_Ap23ue&#39;;
+location.href = 'http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/'+d+'.html&aff_short_key=_Ap23ue';
