776be932cd049863689992c75f74dbd120d10634
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-12-10 13:25:45 +0700

    Create rediraex.js

---
+++ rediraex.js
@@ -0,0 +1,5 @@
+var d=&#39;&#39;;
+var d= window.location.href;
+d=d.replace(&quot;?&quot;, &quot; &quot;);
+d=d.match(/\d{11,20}/);
+location.href = &#39;http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/&#39;+d+&#39;.html&amp;aff_short_key=_Ap23ue&#39;;
