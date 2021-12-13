74e32892d248a15af364ad072871ecb58463cdca
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-12-13 20:36:25 +0700

    Update rediraex.js

--- rediraex.js
+++ rediraex.js
@@ -1,16 +1,5 @@
-var urlmain = document.URL;
-var md = document.URL.split("/")[3];
-var key = document.URL.split("/")[4];
-var id = document.URL.split("/")[5];
-
-if (md == 'deep') {
-  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
-} else { 
-  var redir = 'https://s.click.aliexpress.com/e/'+key;
-}
-
-setTimeout(
-  function(){
-    window.location = redir
-  },
-1000);
+var d='';
+var d= window.location.href;
+d=d.replace("?", " ");
+d=d.match(/\d{11,20}/);
+location.href = 'http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/'+d+'.html&aff_short_key=_9z6YLq';
