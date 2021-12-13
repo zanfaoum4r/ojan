2c6e9c239284734661989a17b88889fbf6da0ac1
zanfaoum4r <76653404+zanfaoum4r@users.noreply.github.com>
2021-12-13 20:26:28 +0700

    Update rediraex.js

--- rediraex.js
+++ rediraex.js
@@ -1,9 +1,12 @@
 var urlmain = document.URL;
 var md = document.URL.split("/")[3];
-var id = document.URL.split("/")[4];
+var key = document.URL.split("/")[4];
+var id = document.URL.split("/")[5];
 
 if (md == 'deep') {
   var redir = 'http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/'+id+'.html&aff_short_key=_9z6YLq';
+} else { 
+  var redir = 'https://s.click.aliexpress.com/e/'+key;
 }
 
 setTimeout(
